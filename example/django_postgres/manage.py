a5a0190293fdaad11d9cba52fa18730ca47d24e8
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2020-11-30 17:15:26 +0200

    Add django example

---
+++ example/django_postgres/manage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+from opentelemetry.instrumentation.django import DjangoInstrumentor
+from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
+
+import uptrace
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_postgres.settings')
+
+    upclient = uptrace.Client(dsn="")
+
+    #Psycopg2Instrumentor().instrument()
+    DjangoInstrumentor().instrument()
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
