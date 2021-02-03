f05538f1cd7a6145a259b0d3a9bd84c46cf4473d
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2021-02-03 17:36:43 +0200

    Update example

--- example/basic/README.md
+++ example/basic/README.md
@@ -4,5 +4,5 @@ To run this example:
 
 ```bash
 pip install -r requirements.txt
-UPTRACE_DSN="https://<key>@uptrace.dev/<project_id>" python3 run.py
+UPTRACE_DSN="https://<key>@uptrace.dev/<project_id>" python3 main.py
 ```
--- example/basic/main.py
+++ example/basic/main.py
@@ -5,6 +5,7 @@ import uptrace
 # Set dsn or UPTRACE_DSN env var.
 upclient = uptrace.Client(dsn="")
 
+# Use upclient to report errors when there are no spans.
 upclient.report_exception(ValueError("Hello from uptrace-python"))
 
 tracer = upclient.get_tracer(__name__)
@@ -12,13 +13,13 @@ tracer = upclient.get_tracer(__name__)
 with tracer.start_as_current_span("main span") as span:
     with tracer.start_as_current_span("child1") as span:
         span.set_attribute("key1", "value1")
-        span.add_event("event-name", {"foo": "bar"})
+        span.record_exception(ValueError("exception1"))
 
     with tracer.start_as_current_span("child2") as span:
         span.set_attribute("key2", "value2")
-        span.add_event("event-name", {"foo": "baz"})
+        span.set_attribute("key3", 123.456)
 
-    print("trace", upclient.trace_url(span))
+    print("trace:", upclient.trace_url(span))
 
 # Flush and close the client.
 upclient.close()
