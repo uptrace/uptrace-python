f9b7d938ff505ea1fb85fea00176eb036f136d6d
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2025-12-12 12:44:24 +0200

    fix: build

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 from nox import session
 
 
-@session(python=["3.8", "3.9", "3.10"], reuse_venv=True)
+@session(python=["3.11", "3.12"], reuse_venv=True)
 def test(session):
     session.install(".")
     session.install("-r", "dev-requirements.txt")
