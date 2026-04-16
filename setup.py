025e88d4fcd2400731083e7f8987c711fe9ce359
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2026-04-16 09:46:18 +0300

    chore: remove setup.py and setup.cfg (replaced by pyproject.toml)

--- setup.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import setuptools
-
-PKG_INFO = {}
-with open(os.path.join("src", "uptrace", "version.py")) as f:
-    exec(f.read(), PKG_INFO)
-
-setuptools.setup(version=PKG_INFO["__version__"])
